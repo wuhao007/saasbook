78cb2fb5e0a8dc00beaa1cd1c33e1955a4a67f4e
Hao Wu <tohaowu@gmail.com>
2015-01-21 19:30:16 -0400

    add change

--- oracle-of-bacon/lib/oracle_of_bacon.rb
+++ oracle-of-bacon/lib/oracle_of_bacon.rb
@@ -21,10 +21,14 @@ class OracleOfBacon
 
   def from_does_not_equal_to
     # YOUR CODE HERE
+    errors.add(:from, 'cannot be the same as To') unless @from != @to
   end
 
-  def initialize(api_key='')
+  def initialize(api_key='', from = 'Kevin Bacon', to = 'Kevin Bacon')
     # your code here
+    @api_key = api_key
+    @from = from
+    @to = to
   end
 
   def find_connections
@@ -37,13 +41,16 @@ class OracleOfBacon
       # convert all of these into a generic OracleOfBacon::NetworkError,
       #  but keep the original error message
       # your code here
+      raise NetworkError
     end
     # your code here: create the OracleOfBacon::Response object
+    Response.new(xml)
   end
 
   def make_uri_from_arguments
     # your code here: set the @uri attribute to properly-escaped URI
     #   constructed from the @from, @to, @api_key arguments
+    @uri = 'http://oracleofbacon.org/p=' + @api_key + '&a=' + CGI.escape(@from) + '&b=' + CGI.escape(@to)
   end
       
   class Response
@@ -62,6 +69,17 @@ class OracleOfBacon
       # your code here: 'elsif' clauses to handle other responses
       # for responses not matching the 3 basic types, the Response
       # object should have type 'unknown' and data 'unknown response'         
+      elsif ! @doc.xpath('/spellcheck').empty?
+        @type = :spellcheck
+        @data = @doc.xpath('//match').map(&:content)
+      elsif ! @doc.xpath('/link').empty?
+        @type = :graph
+        actors = @doc.xpath('//actor').map(&:content)
+        movies = @doc.xpath('//movie').map(&:content)
+        @data = actors.zip(movies).flatten.compact
+      else
+        @type = :unknown
+        @data = 'unknown response'
       end
     end
     def parse_error_response
